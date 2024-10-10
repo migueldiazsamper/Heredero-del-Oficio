5c51c149b12c7c1ccf506116d532bfbf016feccf
Miguel "Bitem" Díaz <migueldiazsamper@gmail.com>
2024-10-10 13:10:30 +0200

    First approach towards finishing minigame 2 rotations

--- Assets/Scripts/DraggableItem.cs
+++ Assets/Scripts/DraggableItem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DraggableItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerClickHandler
 {
     public ItemSlot currentSlot; // Referencia al slot actual en el que se encuentra el objeto
 
@@ -11,11 +11,18 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, 
     private CanvasGroup canvasGroup;
     private Canvas canvas;
 
+    private int currentRotation = 0; // Rotación actual del objeto
+    [ SerializeField ] private int correctRotation = 3;
+    int initialRotation;
+
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
         canvas = GetComponentInParent<Canvas>();
+        initialRotation = Random.Range(0, 4);
+        currentRotation = initialRotation;
+        rectTransform.Rotate(0, 0, initialRotation * 90);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -47,6 +54,33 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, 
             float randomX = Random.Range(-Screen.width / 2, Screen.width / 2);
             float randomY = Random.Range(-Screen.height / 2, Screen.height / 2);
             rectTransform.anchoredPosition = new Vector2(randomX, randomY);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Si el objeto está en un slot, rota 90º en sentido horario
+        if (currentSlot != null)
+        {
+            rectTransform.Rotate(0, 0, -90);
+            if ( ++currentRotation > 3 )
+            {
+                currentRotation = 0;
+            }
+
+            switch ( currentRotation )
+            {
+                case 0:
+                    rectTransform.Rotate(0, 0, 0);
+                    break;
+                case 1:
+                    rectTransform.Rotate(0, 0, 90);
+            }
+
+            if ( currentRotation == correctRotation )
+            {
+                Debug.Log("Correcto");
+            }
         }
     }
 }
