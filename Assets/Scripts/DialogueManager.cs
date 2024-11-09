012d2bfa6de12751021bb1982ebc2ef9b91d1ada
Miguel "Bitem" Díaz <migueldiazsamper@gmail.com>
2024-11-09 21:14:47 +0100

    Creating dialogues

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Ink.Runtime;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [ Header( "Dialogue UI" ) ]
+    [ SerializeField ] private GameObject dialoguePanel;
+    [ SerializeField ] private TextMeshProUGUI dialogueText;
+
+    private Story currentStory;
+    private bool dialogueIsPlaying = false;
+    private static DialogueManager instance;
+
+    private void Awake ()
     {
-        
+        if ( instance == null )
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy( this.gameObject );
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static DialogueManager GetInstance ()
     {
-        
+        return instance;
     }
 }
