6ee5083d08f68e6d2878b8c29f3f26d1a5a1b26d
Miguel "Bitem" Díaz <migueldiazsamper@gmail.com>
2024-11-24 18:44:20 +0100

    Added main character basic animations

--- Assets/Scripts/MainCharacterManager.cs
+++ Assets/Scripts/MainCharacterManager.cs
@@ -4,33 +4,60 @@ using UnityEngine;
 
 public class MainCharacterManager : MonoBehaviour
 {
-    public Transform characterTransform;
     [SerializeField] float speed = 1.0f;
+    public Transform characterTransform;
     public Transform cameraTransform;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Start()
     {
         characterTransform = GetComponent<Transform>();
+        animator.SetBool("isIdle", true);
+        animator.SetBool("isRunningLeft", false);
+        animator.SetBool("isRunningRight", false);
     }
 
     void Update()
     {
         if ( ! DialogueManager.GetInstance().dialogueIsPlaying )
         {
-            if (Input.GetKey(KeyCode.W))
+            if ( Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) )
             {
-                characterTransform.position += Vector3.up * speed * Time.deltaTime;
+                if (Input.GetKey(KeyCode.W))
+                {
+                    characterTransform.position += Vector3.up * speed * Time.deltaTime;
+                    animator.SetBool("isIdle", false);
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    characterTransform.position += Vector3.down * speed * Time.deltaTime;
+                    animator.SetBool("isIdle", false);
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    characterTransform.position += Vector3.left * speed * Time.deltaTime;
+                    animator.SetBool("isIdle", false);
+                    animator.SetBool("isRunningLeft", true);
+                    animator.SetBool("isRunningRight", false);
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    characterTransform.position += Vector3.right * speed * Time.deltaTime;
+                    animator.SetBool("isIdle", false);
+                    animator.SetBool("isRunningLeft", false);
+                    animator.SetBool("isRunningRight", true);
+                }
             }
-            if (Input.GetKey(KeyCode.S))
+            else
             {
-                characterTransform.position += Vector3.down * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                characterTransform.position += Vector3.left * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                characterTransform.position += Vector3.right * speed * Time.deltaTime;
+                animator.SetBool("isIdle", true);
+                animator.SetBool("isRunningLeft", false);
+                animator.SetBool("isRunningRight", false);
             }
         }
 
