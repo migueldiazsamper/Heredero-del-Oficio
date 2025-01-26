558863bd0b3a8105e8a2d4b5e6462bf1a7910fd4
Miguel "Bitem" Díaz <migueldiazsamper@gmail.com>
2025-01-26 17:23:37 +0100

    Added sprites

--- Assets/Resources/Scripts/SlotInventario.cs
+++ Assets/Resources/Scripts/SlotInventario.cs
@@ -15,14 +15,24 @@ public class SlotInventario : MonoBehaviour
         if ( PhasesManager.instance.currentPhase == 2 && PhasesManager.instance.vecesMina >= 2 ) // saco arena
         {
             slot.GetComponent<Image>().sprite = sacoArena;
+            SetAlpha( 1f );
         }
         else if ( PhasesManager.instance.currentPhase == 12 && PhasesManager.instance.vecesMina == 5 ) // saco pigmento
         {
             slot.GetComponent<Image>().sprite = sacoPigmento;
+            SetAlpha( 1f );
         }
         else
         {
             slot.GetComponent<Image>().sprite = null;
+            SetAlpha( 0f );
         }
+    }
+
+    private void SetAlpha ( float alpha )
+    {
+        Color color = slot.GetComponent<Image>().color;
+        color.a = alpha;
+        slot.GetComponent<Image>().color = color;
     }
 }
